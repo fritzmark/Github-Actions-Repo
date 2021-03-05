8198a860e9b82623ad46b669f6065cd1ed202ea0
Michael Schulz <michael.schulz.mail@gmail.com>
2021-03-05 22:05:40 +0100

    Now making the secret variable known via yml file.

--- src/__tests__/MyTest.test.ts
+++ src/__tests__/MyTest.test.ts
@@ -1,6 +1,6 @@
 describe('Meine Tests', () => {
     it('Mein erster Test', () => {
         expect(true).toBe(true);
-        expect(process.env.MEINE_GEHEIME_VARIABLE).toEqual("Geheimer wert.")
+        expect(process.env.MEINE_GANZ_NORMALE_VARIABLE).toEqual("Geheimer wert.")
     });
 });
