081c6757343b3f3d649d275e07c766a81a307174
Michael Schulz <michael.schulz.mail@gmail.com>
2021-03-05 23:24:28 +0100

    Change to check environment behaviour.

--- src/__tests__/MyTest.test.ts
+++ src/__tests__/MyTest.test.ts
@@ -1,6 +1,5 @@
 describe('Meine Tests', () => {
     it('Mein erster Test', () => {
         expect(true).toBe(true);
-        expect(process.env.MEINE_GANZ_NORMALE_VARIABLE).toEqual("Geheimer wert.")
     });
 });
